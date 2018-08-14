aa01788119051f6ca68eec362e1285e6f0eb4d9e
jacob24 <abello.jacob@gmail.com>
2018-08-14 12:18:33 -0600

    sample hello world app

---
+++ flask-hello-world.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DemoEntity(declarative_base()):
+    __tablename__ = "demo"
+
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    lastname = Column(String)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    try:
+        engine = create_engine('mysql://%s:%s@%s/%s' % ('root', 'ajninedoc_12', 'localhost', 'trinimbus_db'), echo=False)
+        Session = sessionmaker(bind=engine)
+        OrmSession = Session()
+
+        demo = OrmSession.query(DemoEntity).first()
+
+    except Exception as e:
+        raise ValueError(str(e))
+
+    return 'This is a simple flask Hello World! web app, Succesfully connected to database: Welcome %s, %s' % (demo.firstname, demo.lastname)
+
+
+if __name__ == '__main__':
+    app.run()
